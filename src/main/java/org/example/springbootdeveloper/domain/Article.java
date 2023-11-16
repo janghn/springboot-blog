88f50170d4bf351f14a72fa628ddc8ebf13cf315
janghn <102577045+janghn@users.noreply.github.com>
2023-11-16 21:00:09 +0900

    Initial commit

---
+++ src/main/java/org/example/springbootdeveloper/domain/Article.java
@@ -0,0 +1,43 @@
+package org.example.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+    /*protected Article(){        // 기본 생성자
+
+    }
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getContent(){
+        return content;
+    }*/
+}
