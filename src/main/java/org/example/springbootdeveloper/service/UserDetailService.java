5a9e704715f6ec31703d7c10cafedb80ff163079
janghn <102577045+janghn@users.noreply.github.com>
2023-11-29 19:21:03 +0900

    SpringSecurity로 로그인/로그아웃, 회원가입 구현

---
+++ src/main/java/org/example/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package org.example.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.User;
+import org.example.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리테에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
